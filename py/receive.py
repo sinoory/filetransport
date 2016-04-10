06259543c3d8848a469c510c24730e648c0d89e1
sin <sinoory@126.com>
2016-04-10 20:44:48 +0800

    format it

--- py/receive.py
+++ py/receive.py
@@ -2,70 +2,52 @@
 
 from socket import *
 
-import struct
+import struct,os
 
-ADDR = ('192.168.0.108',9876)
+def get_local_ip(ifname): 
+    import socket, fcntl, struct 
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    inet = fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15])) 
+    ret = socket.inet_ntoa(inet[20:24]) 
+    return ret 
 
-BUFSIZE = 1024
+def main(port):
+    ADDR = (get_local_ip("eth0"),23216)
+    BUFSIZE = 1024
+    INFO_FORMAT='128sL128s'
+    FILEINFO_SIZE=struct.calcsize(INFO_FORMAT)
+    recvSock = socket(AF_INET,SOCK_STREAM)
+    recvSock.bind(ADDR)
+    recvSock.listen(True)
+    print "..."
+    conn,addr = recvSock.accept()
+    #print ">>>> ",addr
+    fhead = conn.recv(FILEINFO_SIZE)
 
-FILEINFO_SIZE=struct.calcsize('128s32sI8s')
+    filename,filesize,dstlocal=struct.unpack(INFO_FORMAT,fhead)
+    filename=filename.strip('\00')
+    dstlocal=dstlocal.strip('\00')
+    #print filename,filesize,dstlocal
+    filename = "%s" %os.path.join(dstlocal,filename)
+    fp = open(filename,'wb')
+    restsize = filesize
+    while 1:
+        if restsize > BUFSIZE:
+            filedata = conn.recv(BUFSIZE)
+        else:
+            filedata = conn.recv(restsize)
+        if not filedata: break
+        fp.write(filedata)
+        restsize = restsize-len(filedata)
+        if restsize == 0:
+         break
 
-recvSock = socket(AF_INET,SOCK_STREAM)
+    fp.close()
+    conn.close()
+    recvSock.close()
+    print "<<<"+filename+"... ",
 
-recvSock.bind(ADDR)
 
-recvSock.listen(True)
-
-print "等待连接..."
-
-conn,addr = recvSock.accept()
-
-print "客户端已连接—> ",addr
-
-fhead = conn.recv(FILEINFO_SIZE)
-
-filename,temp1,filesize,temp2=struct.unpack('128s32sI8s',fhead)
-
-#print filename,temp1,filesize,temp2
-
-print filename,len(filename),type(filename)
-
-print filesize
-
-filename = 'new_'+filename.strip('\00') #...
-
-fp = open(filename,'wb')
-
-restsize = filesize
-
-print "正在接收文件... ",
-
-while 1:
-
-    if restsize > BUFSIZE:
-
-        filedata = conn.recv(BUFSIZE)
-
-    else:
-
-        filedata = conn.recv(restsize)
-
-    if not filedata: break
-
-    fp.write(filedata)
-
-    restsize = restsize-len(filedata)
-
-    if restsize == 0:
-
-     break
-
-print "接收文件完毕，正在断开连接..."
-
-fp.close()
-
-conn.close()
-
-recvSock.close()
-
-print "连接已关闭..."
+if __name__=="__main__":
+    main(23216)
+    #print get_local_ip("eth0") 
--- py/sender.py
+++ py/sender.py
@@ -1,41 +1,42 @@
 # -*- coding: utf-8 -*-
-
 from socket import *
-
-import os
-
+import os,sys
 import struct
+from optparse import OptionParser
+def send(filename,ip,dstlocal='~/',port=23216):
+    ADDR = (ip,port)
+    BUFSIZE = 1024
+    INFO_FORMAT='128sL128s'
+    FILEINFO_SIZE=struct.calcsize(INFO_FORMAT)
+    sendSock = socket(AF_INET,SOCK_STREAM)
+    sendSock.connect(ADDR)
+    fhead=struct.pack(INFO_FORMAT,os.path.basename(filename),os.stat(filename).st_size,dstlocal)
+    sendSock.send(fhead)
+    fp = open(filename,'rb')
+    while 1:
+        filedata = fp.read(BUFSIZE)
+        if not filedata: break
+        sendSock.send(filedata)
 
-ADDR = ('192.168.0.108',9876)
+    fp.close()
+    sendSock.close()
+    print "<<<<"
 
-BUFSIZE = 1024
 
-filename = 'wubi.exe'
+def main():                         
+    USAGE="send -f <file> -t <ip:local>  "
+    optP=OptionParser(USAGE)
+    optP.add_option("-f","--file",action="store",type="string",dest="file",default="",\
+                    help="point the file to send")
+    optP.add_option("-t","--ipLocal",action="store",type="string",dest="ipLocal",default="",\
+                    help="to : ip:local ,such as 192.168.0.108:/home/xxx/tmp/")
+    options,ags=optP.parse_args(sys.argv[1:])
+    if options.file!='' and options.ipLocal!='':
+        send(filename=options.file,ip=options.ipLocal.split(":")[0],
+             dstlocal=options.ipLocal.split(":")[1])
+    else :
+        print "Use addlog -h to show detail usage"
 
-FILEINFO_SIZE=struct.calcsize('128s32sI8s')
 
-sendSock = socket(AF_INET,SOCK_STREAM)
-
-sendSock.connect(ADDR)
-
-fhead=struct.pack('128s11I',filename,0,0,0,0,0,0,0,0,os.stat(filename).st_size,0,0)
-
-sendSock.send(fhead)
-
-fp = open(filename,'rb')
-
-while 1:
-
-    filedata = fp.read(BUFSIZE)
-
-    if not filedata: break
-
-    sendSock.send(filedata)
-
-print "文件传送完毕，正在断开连接..."
-
-fp.close()
-
-sendSock.close()
-
-print "连接已关闭..."
+if __name__=="__main__":
+    main()
